22b8604a47450ddfb284c459ac03b1bb09dbfb66
Jake Miller <jake0miller@gmail.com>
2019-09-24 00:19:56 -0600

    Write create route for games

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -15,6 +15,7 @@ router.get("/", function(req, res, next) {
     });
 });
 
+/* GET game by id */
 router.get("/:id", function(req, res, next) {
   Game.findOne({
     where: { id: req.params.id }
@@ -26,6 +27,24 @@ router.get("/:id", function(req, res, next) {
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({error})
+    });
+});
+
+/*POST new game*/
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
     });
 });
 
